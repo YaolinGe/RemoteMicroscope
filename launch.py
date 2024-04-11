3a9bc9244082e2f150642898a11fbce88a52eecc
Yaolin Ge <geyaolin@gmail.com>
2024-04-11 13:05:31 +0200

    update launching method

--- launch.py
+++ launch.py
@@ -1,3 +1,4 @@
+import sys
 from waitress import serve
 import logging
 from src.server import app
@@ -5,8 +6,13 @@ from src.server import app
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Check if the command-line argument is provided
+if len(sys.argv) > 1 and sys.argv[1] == 'server':
+    host = '0.0.0.0'
+else:
+    host = 'localhost'
+
 # Start the server
 logger.info("Starting server...")
-serve(app, host='0.0.0.0', port=5000)
+serve(app, host=host, port=8123)
 logger.info("Server started.")
-
