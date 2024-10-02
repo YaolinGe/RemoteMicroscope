e024e8ac8035beebc9712ced89bf4f5a49db80e4
Yaolin Ge <geyaolin@gmail.com>
2024-10-02 17:15:49 +0200

    make it more robust

--- src/server.py
+++ src/server.py
@@ -16,14 +16,18 @@ class CameraApp:
         self.camera_cache = {}
         self.camera_cache_time = 0
         self.CACHE_DURATION = 600
+        self.camera_status = "initializing"
         self.initialize_app()
+
 
     def initialize_app(self):
         self.app.add_url_rule("/", "stream_camera", self.stream_camera, methods=['GET', 'POST'])
-        self.app.add_url_rule('/capture', 'capture_image', self.capture_image, methods=['POST'])
-        self.app.add_url_rule('/video_feed/<int:camera_index>', 'video_feed', self.video_feed)
-        self.app.add_url_rule('/list_cameras', 'list_all_cameras_to_razor', self.list_all_cameras_to_razor, methods=['GET'])
+        self.app.add_url_rule('/initialize_camera/<int:camera_index>', 'initialize_camera', self.initialize_camera, methods=['POST'])
         self.app.add_url_rule('/ping', 'ping', self.ping, methods=['GET'])
+        self.app.add_url_rule('/camera_status', 'get_camera_status', self.get_camera_status, methods=['GET'])
+        self.app.add_url_rule('/capture', 'capture_image', self.capture_image, methods=['POST'])
+        self.app.add_url_rule('/video_feed/<int:camera_index>', 'video_feed', self.video_feed, methods=['GET'])
+        self.app.add_url_rule('/list_cameras', 'list_all_cameras_to_razor', self.list_all_cameras_to_razor, methods=['GET'])
         self.ping(initialize=True)
         asyncio.run(self.initialize_cameras())
 
@@ -31,6 +35,17 @@ class CameraApp:
         cameras = await asyncio.gather(*(self.setup_camera(i) for i in range(2)))
         self.camera_cache = {i: camera for i, camera in enumerate(cameras) if camera is not None}
         self.camera_cache_time = time.time()
+
+    async def initialize_camera(self, camera_index):
+        camera_info = await self.setup_camera(camera_index)
+        if camera_info:
+            self.camera_cache[camera_index] = camera_info
+            return jsonify({"message": f"Camera {camera_index} initialized"}), 200
+        else:
+            return jsonify({"message": f"Failed to initialize camera {camera_index}"}), 500
+
+    def get_camera_status(self):
+        return str(self.camera_status)
 
     async def setup_camera(self, index):
         try:
@@ -75,7 +90,7 @@ class CameraApp:
             selected_camera = next(iter(self.camera_cache)) if self.camera_cache else 0
         return render_template('camera.html', cameras=self.camera_cache, selected_camera=selected_camera)
 
-    def capture_image(self):
+    async def capture_image(self):
         camera_index = int(request.form.get('camera_index'))
         fileName = request.form.get('filename', 'camera_default')
 
@@ -83,25 +98,29 @@ class CameraApp:
             return "Camera not found", 404
 
         cap = self.camera_cache[camera_index]['cap']
-        ret, frame = cap.read()
 
+        if not cap.isOpened():
+            await asyncio.sleep(0.1)  # Add some delay to allow re-initialization if necessary
+            cap.open(camera_index, self.BACKEND)
+
+        ret, frame = await asyncio.to_thread(cap.read)
         if not ret or frame is None:
             return "Failed to capture image.", 500
 
+        # Save the image asynchronously
         filename = os.path.join(os.getcwd(), 'src', 'static', 'images', f'{fileName}.jpg')
-        cv2.imwrite(filename, frame)
-
-        _, buffer = cv2.imencode('.jpg', frame)
+        await asyncio.to_thread(cv2.imwrite, filename, frame)
+        
+        _, buffer = await asyncio.to_thread(cv2.imencode, '.jpg', frame)
         base64_image = base64.b64encode(buffer).decode('utf-8')
 
-        actual_width = self.camera_cache[camera_index]['width']
-        actual_height = self.camera_cache[camera_index]['height']
+        cap.release()
 
         return jsonify({
             "message": "Image captured successfully",
             "fileName": f"{fileName}.jpg",
             "imageData": base64_image,
-            "resolution": f"{actual_width}x{actual_height}"
+            "resolution": f"{self.camera_cache[camera_index]['width']}x{self.camera_cache[camera_index]['height']}"
         })
 
     async def list_all_cameras_to_razor(self):
@@ -129,21 +148,29 @@ class CameraApp:
         return None
 
     def generate_frames(self, camera_index):
+        self.camera_status = "loading"
         cap = cv2.VideoCapture(camera_index, self.BACKEND)
 
         if not cap.isOpened():
+            self.camera_status = "not available"
             return "Camera not found", 404
 
-        while True:
-            success, frame = cap.read()
-            if not success:
-                break
-            else:
-                ret, buffer = cv2.imencode('.jpg', frame)
-                frame = buffer.tobytes()
+        try:
+            while True:
+                self.camera_status = "streaming"
+                success, frame = cap.read()
+                if not success:
+                    print("Failed to read frame")
+                    break
+                else:
+                    ret, buffer = cv2.imencode('.jpg', frame)
+                    frame = buffer.tobytes()
 
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                    yield (b'--frame\r\n'
+                           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        finally:
+            cap.release()
+            self.camera_status = "completed"
 
     def video_feed(self, camera_index):
         return Response(self.generate_frames(camera_index), mimetype='multipart/x-mixed-replace; boundary=frame')
